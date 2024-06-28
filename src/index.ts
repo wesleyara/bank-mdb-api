a407bd15a5c3a25785f1dc21e3aea66b067b4a1d
wesleyara <wes1gom@gmail.com>
2024-06-28 13:12:20 -0300

    chore: Update swagger document import path in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import express from "express";
 const app = express();
 
 import swaggerUi from "swagger-ui-express";
-const swaggerDocument = require("./swagger.json");
 
 import cors from "cors";
 import dotenv from "dotenv";
@@ -10,6 +9,7 @@ dotenv.config();
 
 import router from "./routes";
 import { connectToDatabase } from "./lib/db";
+const swaggerDocument = require("../public/swagger.json");
 
 const PORT = process.env.PORT || 3333;
 
@@ -39,6 +39,8 @@ connectToDatabase()
 
 app.on("ready", () => {
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(
+      `Server running on port ${PORT}. Access http://localhost:${PORT} to see the documentation.`,
+    );
   });
 });
